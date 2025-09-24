aa8d80f9dc9515b8395fd01d204051278ee7b51c
Arseniy Korobenko <72646905+ArseniyKorobenko@users.noreply.github.com>
2025-09-24 04:02:59 +0300

    Add reset (massively boosts robustness)

--- sat.c
+++ sat.c
@@ -29,8 +29,7 @@ I usr_clause(I,I*),solve();V dump(),reset();I pLit(I);L vLit(I); // also see ini
 #define arrlen(a) (I)(sizeof(a)/sizeof*(a))
 #define ANL(b) AS(sizeof(b.a)==sizeof(V*)||b.n<arrlen(b.a));
 #define pf(e...) printf(e);
-#define D(a) pf(#a "=%x\n",(I)a);
-#define swp(a,b) r(a,a=b;b=r)
+#define D(a) pf(#a "=%d\n",(I)a);
 #define sn(n) b##n(gA.sn,v/2)
 #define p(b,v) r(b.n++,ANL(b);b.a[r]=v)
 #define wv gW[v]
@@ -38,7 +37,6 @@ I usr_clause(I,I*),solve();V dump(),reset();I pLit(I);L vLit(I); // also see ini
 #define ca (gC.a+c+1)
 #define c02(e) {L v=*ca^1,b=ca[1];{e;}v=ca[1]^1;b=*ca;{e;}}
 // TODO: minimization
-// TODO: restarts
 // TODO: binary clauses
 // TODO: unsat proofs
 // TODO: growing gC.end
@@ -53,17 +51,17 @@ Z V init(V){AS(gV.n<VARS);gC.t=gC.x=gC.temp=0x100000;gC.end=gC.temp+0x200000;}
 // V reset(V){gLv.n=0;gQ.n=0;gV.n=0;gH.n=0;} // TODO:
 I pLit(N){R n&1?-n/2:n/2;}L vLit(N){R r(n<0?-n*2|1:n*2,AS(r<LITS));}
 Z I mn(I a,I b){R a<b?a:b;}Z I mx(I a,I b){R a>b?a:b;}
-Z I rng(V){Z u64 r=12345;r^=r<<13;r^=r>>7;r^=r<<17;R r&_;} // xorshift
+Z I rng(V){Z u64 r=12346;r^=r<<13;r^=r>>7;r^=r<<17;R r&_;} // xorshift
 Z Watcher wat(I c,L b){Watcher r={c,b};R r;}
 Z V bset(u64*a,N){a[n/64]|=1ull<<n%64;}Z I bget(u64*a,N){R a[n/64]>>n%64&1;}
 Z V bclr(u64*a,N){a[n/64]&=~(1ull<<n%64);}
 Z I aav(L v){R 3&gA.av[v/64]>>v/2*2%64;}Z I av(L v){R aav(v)^v&1;} // 2t 3f 0? 1?
-Z V hp_up(I i){I v=gH.ai,k=(i-1)/2; W(i&&gV.a[v]>gV.a[gH.ak])
-  {gH.ai=gH.ak;gH.b[gH.ak]=i+1;i=k;k=(k-1)/2;}gH.ai=v;gH.b[v]=i+1;}
+Z V hp_up(I i){I*a=gH.a;I v=ai,k=(i-1)/2; W(i&&gV.a[v]>gV.a[ak])
+  {ai=ak;gH.b[ak]=i+1;i=k;k=(k-1)/2;}ai=v;gH.b[v]=i+1;}
 Z V hp_down(I i){I*a=gH.a;I v=ai;W(i*2+1<gH.n){I j=i*2+1,r=i*2+2;
   I k=r<gH.n&&gV.a[a[r]]>gV.a[a[j]]?r:j;if(gV.a[ak]<=gV.a[v])B;ai=ak;gH.b[ai]=i+1;i=k;}ai=v;}
 Z V hp_put(L v){v/=2;if(gH.b[v])R;gH.b[v]=gH.n;hp_up(p(gH,v));}
-Z I hp_pop(){R gH.n?2*r(*gH.a,*gH.a=gH.a[--gH.n];gH.b[*gH.a]=1;gH.b[r]=0;if(gH.n>1)hp_down(0)):0;}
+Z I hp_pop(){I*a=gH.a;R gH.n?2*r(*a,*a=a[--gH.n];gH.b[*a]=1;gH.b[r]=0;if(gH.n>1)hp_down(0)):0;}
 Z V assign(L v){AS(aav(v)==0);gA.av[v/64]|=(u64)(2|v%2)<<v/2*2%64;}
 Z V enq(L v,I c){assign(v);gV.c[v/2]=c;gV.lv[v/2]=gLv.n;p(gQ,v);}
 Z V watch1(I c,L v,L b){N=wv.n;n=!n?8:!(n&(n-1|7))?n*2:n; // 0,8,16,32,..
@@ -94,11 +92,13 @@ Z I analyze(I c,I*lv){I r=p(gC,0);p(gC,0);I deps=0,i=gQ.n;L v=0;
    vbump(v/2);sn(set);if(gV.lv[v/2]<gLv.n)p(gC,v);else deps++;});
   W(v=gQ.a[--i],!sn(get));c=gV.c[v/2];sn(clr);}W(--deps>0);
  c=r;cn=gC.n-r-1;L*a=ca;*a=v^1;i(cn,gA.sn[ai/128]=0);if(cn==1){*lv=0;R c;}
- i=1;k(2,cn,if(gV.lv[ak/2]>gV.lv[ai/2])i=k);*lv=gV.lv[ai/2];swp(a[1],ai);R c;}
-I solve(){init();i(attempts,I conf=prop();if(conf!=_){
+ i=1;k(2,cn,if(gV.lv[ak/2]>gV.lv[ai/2])i=k);*lv=gV.lv[ai/2];r(a[1],a[1]=ai;ai=r);R c;}
+I solve(){init();I confs=0,RST=1000;i(attempts,I conf=prop();if(conf!=_){
  /* pf("conflict: %d\n",conf) */if(gLv.n==0)R 0;I lv=0;I c=analyze(conf,&lv);
- back_to(lv);c=clause(c,0);if(c!=_)enq(*ca,c);vdecay();
+ back_to(lv);c=clause(c,0);if(c!=_)enq(*ca,c);vdecay();if(++confs>RST){
+ i(gV.n,gV.ai=rng()/(_/15.f));for(I i=gH.n/2-1;i>=0;)hp_down(i--);RST*=1.1;back_to(confs=0);}
  }else{L v=branch();if(!v)R 1;p(gLv,gQ.n);enq(v,_);});back_to(0);R _;}
+
 V dump(V){
     pf("<Heap>\n")i(gH.n,pf("%d ",gH.ai));
     pf("\n<Queue>\n")i(gQ.n,I r=gV.c[gQ.ai/2];pf("%d=>%d|%d  ",r==_?-1:r,pLit(gQ.ai),gV.lv[gQ.ai/2]));
